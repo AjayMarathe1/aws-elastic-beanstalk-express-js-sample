33fec159f0e903000dc03f4a496cc5955ce979d3
AjayMarathe1 <ajaymarathe2237@gmail.com>
2024-04-22 18:02:30 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! hi im ajay marathe from DevOps team as a DevOps Engineer'));
+app.get('/', (req, res) => res.send('Hello World! hi im ajay marathe from DevOps team as a DevOps Engineer. "DevOps is a culture and we have to follow that culture"'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
